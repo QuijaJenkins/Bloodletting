5d343a6cb4890f2a9a6cb0e0fde8bba5cec712ce
ejonath <152559538+ejonath@users.noreply.github.com>
2025-05-05 19:01:54 -0400

    pushed updated menu_track

--- Team4_Bloodletting/Assets/Scripts/GameTimer.cs
+++ Team4_Bloodletting/Assets/Scripts/GameTimer.cs
@@ -1,38 +1,38 @@
-using System.Collections.Generic;
-using System.Collections;
-using UnityEngine;
-using UnityEngine.UI;
+// using System.Collections.Generic;
+// using System.Collections;
+// using UnityEngine;
+// using UnityEngine.UI;
 
-public class GameTimer : MonoBehaviour {
-    public float timeRemaining = 60f; // 1 minute in seconds
-    public Text timerText; 
+// public class GameTimer : MonoBehaviour {
+//     public float timeRemaining = 60f; // 1 minute in seconds
+//     public Text timerText; 
 
-    private bool timerRunning = true;
+//     private bool timerRunning = true;
 
-    void Update()
-    {
-        if (timerRunning)
-        {
-            if (timeRemaining > 0)
-            {
-                timeRemaining -= Time.deltaTime;
-                UpdateTimerDisplay();
-            }
-            else
-            {
-                timeRemaining = 0;
-                timerRunning = false;
-                UpdateTimerDisplay();
-                // Optional: Do something when time is up
-                Debug.Log("Time's up!");
-            }
-        }
-    }
+//     void Update()
+//     {
+//         if (timerRunning)
+//         {
+//             if (timeRemaining > 0)
+//             {
+//                 timeRemaining -= Time.deltaTime;
+//                 UpdateTimerDisplay();
+//             }
+//             else
+//             {
+//                 timeRemaining = 0;
+//                 timerRunning = false;
+//                 UpdateTimerDisplay();
+//                 // Optional: Do something when time is up
+//                 Debug.Log("Time's up!");
+//             }
+//         }
+//     }
 
-    void UpdateTimerDisplay()
-    {
-        int minutes = Mathf.FloorToInt(timeRemaining / 60);
-        int seconds = Mathf.FloorToInt(timeRemaining % 60);
-        timerText.text = string.Format("{0}:{1:00}", minutes, seconds);
-    }
-}
+//     void UpdateTimerDisplay()
+//     {
+//         int minutes = Mathf.FloorToInt(timeRemaining / 60);
+//         int seconds = Mathf.FloorToInt(timeRemaining % 60);
+//         timerText.text = string.Format("{0}:{1:00}", minutes, seconds);
+//     }
+// }
